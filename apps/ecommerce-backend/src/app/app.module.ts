2a9b66a0401fd6d7a7a9c3ac2bff43966208969d
Aleš Orel <aleso@comland.si>
2023-02-22 12:20:20 +0100

    Initial commit.

--- apps/ecommerce-backend/src/app/app.module.ts
+++ apps/ecommerce-backend/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ProductsController, ProductsService } from '@nx-ecommerce/products';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ProductsController],
+  providers: [AppService, ProductsService],
 })
 export class AppModule {}
